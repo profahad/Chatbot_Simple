975ded125b274ed50de33c7e9db5e4c2bd19f240
Muhammad Fahad <mfahad4010@gmail.com>
2018-09-27 12:28:51 +0500

    fix bugs and add functionality

--- Chatbot.py
+++ Chatbot.py
@@ -2,14 +2,28 @@ from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 import os
 
-bot = ChatBot('testing')
+bot = ChatBot('Bot',
+                  storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch'
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.70,
+            'default_response': 'I do not understand.'
+        }
+    ],
+    trainer='chatterbot.trainers.ListTrainer')
 bot.set_trainer(ListTrainer)
 for _file in os.listdir('conversations'):
     conv =  open('conversations/' + _file, 'r').readlines()
     bot.train(conv)
 while True:
     request = input('You : ')
-    response = bot.get_response(request)
-    print('Bot : ', response)
-    if response == 'take care':
+    if request.strip() != 'Bye':
+        response = bot.get_response(request)
+        print('Bot : ', response)
+    elif request.strip() == 'Bye':
+        print('Bot : ', 'Bye')
         break
